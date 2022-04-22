0990d26875a7ffab6843bd01659d50399ab9734c
Jacinth Daniel Moses <jackdanielmoseshs@gmail.com>
2022-04-22 16:28:19 +0000

    Basic Template Implimented

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const app = express()
-const port = 4000;
+var express = require('express');
+var app = express();
 
-app.use(express.static('public'));
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', (request, response) => {
-    //response.status(200).send
-    res.render('index');
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
-const server = app.listen(port, () => {
- console.log(`Cart app listening at http://localhost:${port}`);
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
 });
 
+//close page
+app.get('/close', function(req,res){
+  console.log('Server is Exiting');
+});
 
-//app.use(express.static(path.join(__dirname, '../www')));
+app.listen(3000);
+console.log('Server is listening on port  http://localhost:3000');
